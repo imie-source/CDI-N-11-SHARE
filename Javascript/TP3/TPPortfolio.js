071bbee227ceebd93cb20c32b7acd2f2d928f0f5
simon <simon.louvet@imie.fr>
2015-07-23 12:31:02 +0200

    aide TP3 : fileReader proffessionalisé+callback

--- Javascript/TP3/TPPortfolio.js
+++ Javascript/TP3/TPPortfolio.js
@@ -1,30 +1,15 @@
 $(function () {
-    $('#fileSelector').on('change', function (e) {
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            for (i = 0; i < e.target.files.length; i++) {
 
-                var picReader = new FileReader();
-                picReader.addEventListener("load", function (event) {
-                    var reader = event.target;
-                    $('#preview').append($(
-                            '<div style="display:inline-block;border-style:solid;padding:5px;margin:5px;">'
-                        ).append($('<div>').text(reader.file.name))
-                        .append($(
-                            '<img>'
-                        ).attr('src', reader.result)));
-                    console.log(reader.result);
-                });
+    var callBackFileRead = function (file, data) {
+        console.log('draw');
+        $('#preview').append($(
+                '<div style="display:inline-block;border-style:solid;padding:5px;margin:5px;">'
+            ).append($('<div>').text(file.name))
+            .append($(
+                '<img>'
+            ).attr('src', data)));
+    }
+    var fileReader = fr.imie.inputFileListener.build();
+    fileReader.listen('#fileSelector', callBackFileRead);
 
-                var f = e.target.files[i];
-                if (f.type && !f.type.match('image.*')) {
-                    continue;
-                }
-                picReader.file = f;
-                picReader.readAsDataURL(f);
-
-            }
-        } else {
-            console.log('non supporté');
-        }
-    });
 });
---
+++ Javascript/TP3/fr/imie/inputFileListener.js
@@ -0,0 +1,37 @@
+var fr =fr?fr:{};
+fr.imie = fr.imie||{};
+fr.imie.inputFileListener = {};
+fr.imie.inputFileListener.proto = {
+    listen: function (selector, callback) {
+        $(selector).on('change', function (e) {
+            console.log('change');
+            if (window.File && window.FileReader && window.FileList && window.Blob) {
+                for (var i = 0; i < e.target.files.length; i++) {
+
+                    var picReader = new FileReader();
+                    picReader.addEventListener("load", function (event) {
+                        var reader = event.target;
+                        callback(reader.file, reader.result);
+                    });
+
+                    var f = e.target.files[i];
+                    if (!f.type || !f.type.match('image.*')) {
+                        continue;
+                    }
+                    picReader.file = f;
+                    picReader.readAsDataURL(f);
+
+                }
+            } else {
+                console.log('non supporté');
+            }
+        });
+
+    }
+}
+fr.imie.inputFileListener.state = {};
+fr.imie.inputFileListener.build = function () {
+    return Object.create(fr.imie.inputFileListener.proto, fr.imie.inputFileListener.state);
+};
+
+
